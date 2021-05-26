43833e3a482e9d4e797d995c23d21d57d65c399b
sarvesh-dongre <76567149+sarvesh-dongre@users.noreply.github.com>
2021-05-26 22:30:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preLoad() {
+
+}
+
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+
+
+function draw() {
+    image(video, 9, 0, 640, 480);
+    tint(tint_color);
+}
+
+
+function take_snapshot() {
+    save("student_name.png");
+}
+
+
+function filter_tint() {
+    tint_color = document.getElementById("color_name").value;
+}
